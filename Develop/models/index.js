05de09496c0580f361a328211580522238c4ac2b
jannecook <135638400+jannecook@users.noreply.github.com>
2023-12-10 22:16:49 -0800

    updated models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Products.belongTo(Category, {
+  foreignKey: 'product_id',
+})
 // Categories have many Products
-
+Category.hasMany(Products, {
+  foreignKey: 'product_id',
+})
 // Products belongToMany Tags (through ProductTag)
-
+Products.belongToMany(Tags, {
+  foreignKey: 'product_id',
+})
 // Tags belongToMany Products (through ProductTag)
+Tags.belongToMany(Products, {
+  foreignKey: 'product_tag',
+})
 
 module.exports = {
   Product,
